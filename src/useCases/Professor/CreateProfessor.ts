cc14323b2fe91f8b68f8d76440704d7f8e427150
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-03-15 17:51:47 -0400

    CorrectImplementation

--- src/useCases/Professor/CreateProfessor.ts
+++ src/useCases/Professor/CreateProfessor.ts
@@ -1,14 +1,34 @@
-import { collection, doc, setDoc } from "firebase/firestore";
+import { arrayUnion, collection, doc, setDoc, updateDoc } from "firebase/firestore";
 import { UsersDatabase } from "../../database/Firebase";
 import { Professor } from "../../entities/Professors";
 import { CreateProfessorDTO } from "./CreateProfessorDTO";
 import { FindProfessorByLogin } from "./GetProfessors";
-import { InsertProfessorToClass } from "./InsertProfessorToClass/InsertProfessorToClass";
+
 const CreateProfessor = async (props: CreateProfessorDTO) => {
   const professor = new Professor(props);
+
   if ((await FindProfessorByLogin(props.login)).exists()) {
     throw new Error("Username already taken");
   }
+  const InserProfessorInClasses = async (
+    name: string,
+    _id: string | undefined,
+    discipline: string,
+    _classes: string[]
+  ) => {
+    _classes.forEach((_class) => {
+      const docRef = doc(UsersDatabase, "Turmas", _class);
+      updateDoc(docRef, {
+        Professores: arrayUnion({
+          _id: _id || "#",
+          Name: name,
+          Discipline: discipline,
+        }),
+      }).then(() => {
+        console.log("Inserido em ", _class);
+      });
+    });
+  };
   const InsertProfessorInDataBase = async () => {
     try {
       const insertedDoc = await setDoc(doc(collection(UsersDatabase, "Professores"), professor.login), {
@@ -20,7 +40,7 @@ const CreateProfessor = async (props: CreateProfessorDTO) => {
         Username: professor.login,
         Discipline: professor.discipline,
       });
-      await InsertProfessorToClass(professor.name, professor.discipline, professor._classes, professor.id);
+      await InserProfessorInClasses(professor.name, professor.id, professor.discipline, professor._classes);
       return insertedDoc;
     } catch (e) {
       throw new Error("Insuficient information");
